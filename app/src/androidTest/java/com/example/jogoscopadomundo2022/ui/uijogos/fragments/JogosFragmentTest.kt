fc45ea81eb03c7723103810fa35f88fe1388c7f0
rubens23 <rubens_assis@outlook.com.br>
2023-06-06 16:17:05 -0300

    implementando alguns testes de ui

--- app/src/androidTest/java/com/example/jogoscopadomundo2022/ui/uijogos/fragments/JogosFragmentTest.kt
+++ app/src/androidTest/java/com/example/jogoscopadomundo2022/ui/uijogos/fragments/JogosFragmentTest.kt
@@ -6,6 +6,8 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,6 +16,7 @@ import org.junit.runner.RunWith
 import com.example.jogoscopadomundo2022.R
 import org.junit.Test
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
@@ -22,35 +25,83 @@ import com.example.jogoscopadomundo2022.ui.MainActivity
 import org.hamcrest.CoreMatchers.*
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.example.jogoscopadomundo2022.ui.uijogos.adapters.PartidasAdapter
 import org.hamcrest.Matcher
 
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class JogosFragmentTest{
 
-    private lateinit var scenario: FragmentScenario<JogosFragment>
-    private lateinit var recyclerView: RecyclerView
 
-    private val spinnerOptions = arrayOf("Opção 1", "Opção 2", "Opção 3")
+    lateinit var navController: TestNavHostController
+
 
     @Before
     fun setup(){
-        ActivityScenario.launch(MainActivity::class.java)
-        scenario = launchFragmentInContainer<JogosFragment>(themeResId = R.style.Theme_JogosCopaDoMundo2022)
+        navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
 
-        scenario.onFragment{
-            //recyclerView = it.view?.findViewById(R.id.rvMatches)!!
-            //it.setSpinnerAdapter(ArrayAdapter(it.requireContext(), R.layout.drop_down_item, spinnerOptions))
+        val jogosFragment = launchFragmentInContainer<JogosFragment>(null, R.style.Theme_JogosCopaDoMundo2022)
+
+        jogosFragment.onFragment{
+                fragment->
+            navController.setGraph(R.navigation.main_navigation)
+
+            Navigation.setViewNavController(fragment.requireView(), navController)
         }
+
     }
 
     @Test
-    fun testSpinnerSelection(){
-        onView(withId(R.id.tv_spinner)).perform(click())
-        Thread.sleep(1000) // espera 1 segundo
-        onData(anything()).atPosition(0).perform(click())
+    fun fragmentIsVisible(){
 
+
+        assertThat(navController.currentDestination?.id, `is`(R.id.jogosFragment))
+
+
+    }
+
+    @Test
+    fun testSpinnerSelectionPrimeiraRodada(){
+        onView(withId(R.id.tv_spinner)).perform(click())
+        onView(withText("1ª Rodada")).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.tv_spinner)).check(matches(withText("1ª Rodada")))
+
+
+    }
+
+    @Test
+    fun testSpinnerSelectionSegundaRodada(){
+        onView(withId(R.id.tv_spinner)).perform(click())
+        onView(withText("2ª Rodada")).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.tv_spinner)).check(matches(withText("2ª Rodada")))
+
+
+    }
+
+    @Test
+    fun testSpinnerSelectionTerceiraRodada(){
+        onView(withId(R.id.tv_spinner)).perform(click())
+        onView(withText("3ª Rodada")).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.tv_spinner)).check(matches(withText("3ª Rodada")))
+
+
+    }
+
+    @Test
+    fun testRecyclerViewItemClick(){
+        onView(withId(R.id.tv_spinner)).perform(click())
+        onView(withText("1ª Rodada")).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.tv_spinner)).check(matches(withText("1ª Rodada")))
+
+        onView(withId(R.id.rvMatches)).perform(actionOnItemAtPosition<PartidasAdapter.ViewHolder>(1, click()))
+
+        assertThat(navController.currentDestination?.id, `is`(R.id.detalhesJogoFragment))
 
     }
 
